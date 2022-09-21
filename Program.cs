76d8a4f80523b83ab06659b5c5bd0536df6e6f06
LokeshKiran <lokeshkskiran@gmail.com>
2022-09-21 20:40:44 +0530

    [Lokesh]Added:Ability to create binary search tree using given figure

--- Program.cs
+++ Program.cs
@@ -11,14 +11,19 @@ namespace BinarySearchTree
         public static void Main(string[] args)
         {
             BinaryTree tree = new BinaryTree();
-            tree.Add(50);
+            tree.Add(56);
+            tree.Add(30);
+            tree.Add(22);
+            tree.Add(40);
+            tree.Add(11);
+            tree.Add(16);
+            tree.Add(3);
             tree.Add(70);
             tree.Add(60);
-            tree.Add(20);
-            tree.Add(90);
-            tree.Add(10);
-            tree.Add(40);
-            tree.Add(100);
+            tree.Add(95);
+            tree.Add(65);
+            tree.Add(63);
+            tree.Add(67);
 
             tree.TraverseForPreOrder(tree.root);
             Console.WriteLine();
