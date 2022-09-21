c4ca781a8bd0b04644f8b618ad6e2b3a166a0275
LokeshKiran <lokeshkskiran@gmail.com>
2022-09-21 20:25:42 +0530

    [Lokesh]Added:Ability to create a Binary search tree by adding 56,30 & 70

---
+++ BinaryTree.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree
+{
+    public class BinaryTree
+    {
+        public Node root { get; set; }
+        public bool Add(int value)
+        {
+            Node before = null;
+            Node after = this.root;
+            while (after != null)
+            {
+                before = after;
+                if (value < after.data)
+                {
+                    after = after.leftNode;
+                }
+                else if (value > after.data)
+                {
+                    after = after.rightNode;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+           Node newNode = new Node();
+                newNode.data = value;
+                if (this.root == null)
+                {
+                    this.root = newNode;
+                }
+                else
+                {
+                    if (value < before.data)
+                    {
+                        before.leftNode = newNode;
+                    }
+                    else
+                    {
+                        before.rightNode = newNode;
+                    }
+                }
+            return true;
+        }
+        public void TraverseForPreOrder(Node parent)
+        {
+            if (parent != null)
+            {
+                Console.Write("  "+parent.data+"  ");
+                TraverseForPreOrder(parent.leftNode);
+                TraverseForPreOrder(parent.rightNode);
+            }
+
+        }
+
+        public void TraverseForInOrder(Node parent)
+        {
+            if (parent != null)
+            {
+
+                TraverseForInOrder(parent.leftNode);
+
+                Console.Write("  "+parent.data+"  ");
+
+                TraverseForInOrder(parent.rightNode);
+            }
+
+        }
+
+        public void TraverseForPostOrder(Node parent)
+        {
+            if (parent != null)
+            {
+
+                TraverseForPostOrder(parent.leftNode);
+
+                TraverseForPostOrder(parent.rightNode);
+
+                Console.Write("  "+parent.data+"  ");
+            }
+
+
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            BinaryTree tree = new BinaryTree();
+            tree.Add(50);
+            tree.Add(70);
+            tree.Add(60);
+            tree.Add(20);
+            tree.Add(90);
+            tree.Add(10);
+            tree.Add(40);
+            tree.Add(100);
+
+            tree.TraverseForPreOrder(tree.root);
+            Console.WriteLine();
+
+            tree.TraverseForInOrder(tree.root);
+            Console.WriteLine();
+
+            tree.TraverseForPostOrder(tree.root);
+
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
